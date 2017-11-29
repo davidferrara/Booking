20da9776d237629af5d108af89800edce5f5d28e
Brandon Gordon <gordonb2@students.rowan.edu>
2017-11-29 16:41:04 -0500

    Initial commit

---
+++ app/src/main/java/booking/sp/clbooking/EditActivity.java
@@ -0,0 +1,37 @@
+package booking.sp.clbooking;
+
+/**
+ * Created by Ty on 11/5/2017.
+ */
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        final Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
